c0304021b1e72dee08d07e60a1d3795f86f1bfad
Calvin Zheng <47680544+Calvinzheng123@users.noreply.github.com>
2024-10-23 16:19:10 -0400

    Update setup.py

--- Lion/app/setup.py
+++ Lion/app/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Lion",            
+    name="LionAPI",            
     version="0.1.0",          # Version number
     description="A FastAPI-based soccer data API",
     author="Calvin Zheng",    
